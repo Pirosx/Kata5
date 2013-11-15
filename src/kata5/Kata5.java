e5530416078c00cdb9ca9fae8a20bb1be25160df
usuario <usuario@10.22.146.201>
2013-11-15 14:14:22 +0000

    Final2 JFrameChart

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -5,7 +5,7 @@ public class Kata5 {
     public static void main(String[] args) {
         MailLoader loader = new MockMailLoader();
         HistogramBuilder<Mail> builder = new HistogramBuilder<>(loader.load());
-        HistogramViewer<String> viewer = new  HistogramViewer<>();
+        ChartHistogramViewer<String> viewer = new  ChartHistogramViewer<>();
         viewer.show(builder.build(new AttributeExtractor<Mail, String>() {
 
             @Override
